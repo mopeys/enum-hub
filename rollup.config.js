8d97d01670b05e87d5c86943608f9912c5f37d1a
mike.zhu <512122313@qq.com>
2021-03-25 15:36:21 +0800

    config: 配置, 依赖维护

---
+++ rollup.config.js
@@ -0,0 +1,23 @@
+import babel from "rollup-plugin-babel";
+import { uglify } from "rollup-plugin-uglify";
+import pkg from "./package.json";
+export default {
+  input: "src/index.js",
+  output: [
+    {
+      file: pkg.module,
+      format: "es",
+    },
+    {
+      file: pkg.browser,
+      format: "umd",
+      name: "EnumHub",
+    },
+  ],
+  plugins: [
+    babel({
+      exclude: "node_modules/**",
+    }),
+    process.env.NODE_ENV === "production" ? uglify() : null,
+  ],
+};
