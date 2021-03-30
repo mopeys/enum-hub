8f5813443b33a58466ac4ad29527f6a22b663359
mike.zhu <512122313@qq.com>
2021-03-30 17:37:26 +0800

    试图拆分出babel-runtime依赖

--- rollup.config.js
+++ rollup.config.js
@@ -1,22 +1,47 @@
-import babel from "rollup-plugin-babel";
-// import { uglify } from "rollup-plugin-uglify";
+import { babel } from "@rollup/plugin-babel";
+import resolve from "@rollup/plugin-node-resolve";
+// import html from "@rollup/plugin-html";
+// import serve from "rollup-plugin-serve";
+import { uglify } from "rollup-plugin-uglify";
 import pkg from "./package.json";
+
 export default {
   input: "src/index.js",
+  external: [/@babel\/runtime/],
   output: [
     {
       file: pkg.module,
       format: "es",
     },
     {
+      file: pkg.main,
+      format: "cjs",
+      exports: "default",
+    },
+    {
       file: pkg.browser,
       format: "umd",
       name: "EnumHub",
     },
-  ],
+  ].map((opt) => ({
+    globals: {
+      "@babel/runtime/regenerator": "regeneratorRuntime",
+      "@babel/runtime/helpers/asyncToGenerator": "asyncToGenerator",
+      "@babel/runtime/helpers/classCallCheck": "classCallCheck",
+      "@babel/runtime/helpers/createClass": "createClass",
+      "@babel/runtime/helpers/defineProperty": "defineProperty",
+    },
+    ...opt,
+  })),
+
   plugins: [
+    // serve({
+    //   port: "3000",
+    // }),
+    resolve(),
     babel({
       exclude: "node_modules/**",
+      babelHelpers: "runtime",
     }),
     // process.env.NODE_ENV === "production" ? uglify() : null,
   ],
