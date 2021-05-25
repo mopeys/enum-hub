e0bb61ce77271ccc10b964a14c206ecb2bd60fd8
mike.zhu <512122313@qq.com>
2021-05-25 11:03:01 +0800

    🔧 build: 调整rollup配置

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,5 @@
 import { babel } from "@rollup/plugin-babel";
 import resolve from "@rollup/plugin-node-resolve";
-// import html from "@rollup/plugin-html";
-// import serve from "rollup-plugin-serve";
-// import { uglify } from "rollup-plugin-uglify";
 import pkg from "./package.json";
 
 export default {
@@ -18,31 +15,17 @@ export default {
       format: "cjs",
       exports: "default",
     },
-    {
-      file: pkg.browser,
-      format: "umd",
-      name: "EnumHub",
-    },
-  ].map((opt) => ({
-    globals: {
-      "@babel/runtime/regenerator": "regeneratorRuntime",
-      "@babel/runtime/helpers/asyncToGenerator": "asyncToGenerator",
-      "@babel/runtime/helpers/classCallCheck": "classCallCheck",
-      "@babel/runtime/helpers/createClass": "createClass",
-      "@babel/runtime/helpers/defineProperty": "defineProperty",
-    },
-    ...opt,
-  })),
-
+    // {
+    //   file: pkg.browser,
+    //   format: "umd",
+    //   name: "EnumHub",
+    // },
+  ],
   plugins: [
-    // serve({
-    //   port: "3000",
-    // }),
     resolve(),
     babel({
       exclude: "node_modules/**",
       babelHelpers: "runtime",
     }),
-    // process.env.NODE_ENV === "production" ? uglify() : null,
   ],
 };
