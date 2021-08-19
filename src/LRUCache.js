3963afe35d232e3ee851479c26b6ae26efae443a
mike.zhu <512122313@qq.com>
2021-08-19 10:18:28 +0800

    ✨ feat(LRU): 实现LRU cache

---
+++ src/LRUCache.js
@@ -0,0 +1,61 @@
+class LinkedNode {
+  constructor(payload) {
+    this.next = null;
+    this.previous = null;
+    this.payload = payload;
+  }
+}
+
+export default class LRUCache {
+  constructor(size) {
+    this.max = Number(size) || Number.MAX_SAFE_INTEGER;
+    this.nodes = Object.create(null);
+    this.head = new LinkedNode();
+    this.tail = new LinkedNode();
+    this.head.next = this.tail;
+    this.tail.previous = this.head;
+  }
+
+  get(key) {
+    const node = this.nodes[key];
+    if (!node) return;
+    this.moveToTop(node);
+    return node.payload.data;
+  }
+
+  set(key, data) {
+    const node = this.nodes[key];
+    if (node) {
+      return this.moveToTop(node);
+    }
+    const newNode = new LinkedNode({ key, data });
+    this.pushToTop(newNode);
+  }
+
+  pushToTop(node) {
+    const l = Reflect.ownKeys(this.nodes).length;
+    if (l >= this.max) {
+      this.delete(this.tail.previous);
+    }
+    this.nodes[node.payload.key] = node;
+    this.head.next.previous = node;
+    node.next = this.head.next;
+    this.head.next = node;
+    node.previous = this.head;
+  }
+  moveToTop(node) {
+    this.delete(node);
+    this.pushToTop(node);
+  }
+
+  delete(node) {
+    node.previous.next = node.next;
+    node.next.previous = node.previous;
+    this.destory(node);
+  }
+  destory(node) {
+    node.previous = null;
+    node.next = null;
+    delete this.nodes[node.payload.key];
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,35 +1,50 @@
-const enums = Symbol("enums");
-const check = Symbol("check");
-const runFetchRemote = Symbol("runFetchRemote");
+import LRUCache from './LRUCache';
 
+const enums = Symbol('enums');
+const check = Symbol('check');
+const runFetchRemote = Symbol('runFetchRemote');
+
+const createProxy = (obj) => {
+  return new Proxy(obj, {
+    get(target, property) {
+      return target.get(property);
+    },
+    set(target, property, value) {
+      target.set(property, value);
+      return true;
+    },
+  });
+};
 /**
  * 获取枚举集合，若本地没有，则从远程获取并缓存
  */
 export default class EnumHub {
   // 收集正在请求中的name
   fetchingNames = {};
-  // 私有枚举集合
-  [enums] = {};
 
   constructor({
     fetchRemote = null, // 获取枚举的远程请求函数
+    max, // 缓存最大个数
   } = {}) {
+    // 私有枚举集合
+    this[enums] = createProxy(new LRUCache(max));
     this[check](fetchRemote);
     this.fetchRemote = fetchRemote;
   }
   // 私有检查
   [check](fetchRemote) {
-    if (!fetchRemote || typeof fetchRemote !== "function") {
-      throw new Error("[EnumHub]: fetchRemote应当为获取枚举的远程请求函数");
+    if (!fetchRemote || typeof fetchRemote !== 'function') {
+      throw new Error('[EnumHub]: fetchRemote应当为获取枚举的远程请求函数');
     }
   }
   // 获取枚举
   async getEnum(name, fetchRemote) {
-    if (!name) throw new Error("[EnumHub]: getEnum需要传入name参数");
-    if (!this[enums][name]) {
+    if (!name) throw new Error('[EnumHub]: getEnum需要传入name参数');
+    const en = this[enums][name];
+    if (!en) {
       return await this[runFetchRemote](name, fetchRemote);
     }
-    return this[enums][name];
+    return en;
   }
 
   // 私有执行fetchRemote函数并缓存枚举结果
